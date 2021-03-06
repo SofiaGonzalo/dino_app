8ae842f959cef3c618cf37bd6df5681bfa7f77cd
sofiagonzalo <sofiagonzaloc@outlook.com>
2021-03-05 19:14:13 -0700

    Inicialización del primer dart

---
+++ lib/description place.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//create a new class
+
+class DescriptionPlace extends StatelessWidget{ // StatelessWidget for static information
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
